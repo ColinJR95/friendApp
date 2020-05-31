a6f6a3be0a665ed6bf3a86015671baeb26162179
ColinJR95 <javcolin1@gmail.com>
2020-05-30 21:29:02 -0700

    Add files via upload

---
+++ build/precache-manifest.293387c118819a485ef1b5530e019343.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98c01e9e6eb8e07a88cb586d73bc0cf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bda4e85e631526da84fb",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "67088e8ade54b4cb08e9",
+    "url": "/static/css/main.a515619a.chunk.css"
+  },
+  {
+    "revision": "bda4e85e631526da84fb",
+    "url": "/static/js/2.deca058e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.deca058e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67088e8ade54b4cb08e9",
+    "url": "/static/js/main.aa07aa34.chunk.js"
+  },
+  {
+    "revision": "857cfae17f1118125766",
+    "url": "/static/js/runtime-main.e77b7633.js"
+  },
+  {
+    "revision": "489bfa2e1ec0e7d56dbbfee4e2181af9",
+    "url": "/static/media/harryp__-webfont.489bfa2e.woff"
+  },
+  {
+    "revision": "ebc52830844b6e50d7cdcc5f6ef71852",
+    "url": "/static/media/harryp__-webfont.ebc52830.woff2"
+  }
+]);
